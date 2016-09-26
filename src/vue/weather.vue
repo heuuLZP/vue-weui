6d39f01a05fbabe815d97fac58df55ea7676ea42
heuuLZP <heuuLZP@163.com>
2016-09-26 15:20:22 +0800

    ###增加请求判断 ##增加c-cloak，避免闪烁

--- src/vue/weather.vue
+++ src/vue/weather.vue
@@ -2,17 +2,17 @@
   <h2>weather</h2>
   <dl>
     <dt>天气</dt>
-    <dd> {{ weather }}</dd>
+    <dd v-cloak> {{ weather }}</dd>
     <dt>温度</dt>
-    <dd>{{ temp }}°</dd>
+    <dd v-cloak>{{ temp }}</dd>
   </dl>
 </template>
 <script>
   export default {
     data(){
       return {
-        weather:"未知",
-        temp:0,
+        weather:"",
+        temp:"",
         apiURL:"https://api.heweather.com/x3/weather?cityid=CN101010100&key=bac728ec43314d76bf2c85e7fa0675d8",
         data:{}
       }
@@ -26,13 +26,21 @@
       getData(){
         this.$http.get(this.apiURL)
           .then((response) => {
-            this.$set('data',response.body['HeWeather data service 3.0'][0]);
-            this.$set('weather',this.data.now.cond.txt)
-            this.$set('temp',this.data.now.tmp)
+          //增加判断
+            if (response.body['HeWeather data service 3.0'][0].status === 'ok'){
+              this.$set('data',response.body['HeWeather data service 3.0'][0]);
+              this.$set('weather',this.data.now.cond.txt)
+              this.$set('temp',this.data.now.tmp + '°')
+            }
+            },(response) => {
+              console.log("数据错误")
             })
       }
     }
   }
 </script>
 <style>
+[v-cloak]{
+  display:none;
+}
 </style>
